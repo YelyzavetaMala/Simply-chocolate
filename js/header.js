99ef16a52bafe422b5f8fb95586c49588036aef3
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-07-09 21:10:59 +0300

    mob

---
+++ js/header.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.header-button');
+const modal_button_close = document.querySelector('.modal-button-close');
+const header_modal = document.getElementById('header-modal');
+
+burger.addEventListener('click', () => {
+  header - modal.showModal();
+});
+
+modal -
+  button -
+  close.addEventListener('click', () => {
+    header - modal.close();
+  });
