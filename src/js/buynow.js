c0e44b3df95f9e6060652257ef2d037f21852056
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-04-18 21:54:17 +0300

    add form order

--- src/js/buynow.js
+++ src/js/buynow.js
@@ -1,10 +1,12 @@
-const buynowBtn = document.getElementById('buynowBtn');
+const buynowBtns = document.querySelectorAll('.buynowBtn');
 const buynowPopup = document.getElementById('buynowPopup');
 const buynowClose = document.getElementById('buynowClose');
 
-buynowBtn.addEventListener('click', () => {
-  buynowPopup.style.display = 'flex';
-  document.body.classList.add('noscroll');
+buynowBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    buynowPopup.style.display = 'flex';
+    document.body.classList.add('noscroll');
+  });
 });
 
 buynowClose.addEventListener('click', () => {
